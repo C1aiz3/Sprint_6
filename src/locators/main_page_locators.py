5d2d2de9517c325d9b0b96aa48ba8532f6cd064c
Pavel Tishenko <pavelclaize@gmail.com>
2025-03-31 20:11:19 +0300

    Refactored locators

--- src/locators/main_page_locators.py
+++ src/locators/main_page_locators.py
@@ -1,28 +1,27 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators:
-    cookie_button = By.XPATH, '//button[@id="rcc-confirm-button"]'                          #Кнопка принятия куки
-    first_question = By.XPATH, '//div[@class="accordion"]/div[1]'                           #Первый вопрос на главной странице
-    second_question = By.XPATH, '//div[@class="accordion"]/div[2]'                          #Второй вопрос на главной странице
-    third_question = By.XPATH, '//div[@class="accordion"]/div[3]'                           #Третий вопрос на главной странице
-    fourth_question = By.XPATH, '//div[@id="accordion__heading-3"]'                         #Четвертый вопрос на главной странице
-    fiveth_question = By.XPATH, '//div[@id="accordion__heading-4"]'                         #Пятый вопрос на главной странице
-    sixth_question = By.XPATH, '//div[@id="accordion__heading-5"]'                          #Шестой вопрос на главной странице
-    seventh_question = By.XPATH, '//div[@id="accordion__heading-6"]'                        #Седьмой вопрос на главной странице
-    eighth_question = By.XPATH, '//div[@id="accordion__heading-7"]'                         #Восьмой вопрос на главной странице
-    first_answer = By.XPATH, '//div[@class="accordion"]/div[1]//p'                          #Ответ на первый вопрос
-    second_answer = By.XPATH, '//div[@class="accordion"]/div[2]//p'                         #Ответ на второй вопрос
-    third_answer = By.XPATH, '//div[@class="accordion"]/div[3]//p'                          #Ответ на третий вопрос
-    fourth_answer = By.XPATH, '//div[@class="accordion"]/div[4]//p'                         #Ответ на четвертый вопрос
-    fiveth_answer = By.XPATH, '//div[@class="accordion"]/div[5]//p'                         #Ответ на пятый вопрос
-    sixth_answer = By.XPATH, '//div[@class="accordion"]/div[6]//p'                          #Ответ на шестой вопрос
-    seventh_answer = By.XPATH, '//div[@class="accordion"]/div[7]//p'                        #Ответ на седьмой вопрос
-    eighth_answer = By.XPATH, '//div[@class="accordion"]/div[8]//p'                         #Ответ на восьмой вопрос
-    top_order_button = By.XPATH, '//button[@class="Button_Button__ra12g"]'                  #Кнопка "Заказать" сверху
-    bottom_order_button = By.XPATH, '//div[@class="Home_FinishButton__1_cWm"]/button'       #Кнопка "Заказать" снизу
-    dzen_button = By.XPATH, '//a[@target="_blank"]'                                         #Кнопка "Дзен"
-    alert = By.XPATH, '//div[@class="kc00720f4"]'                                           #Поисковая строка на Дзене
-    scooter_main_page_button = By.XPATH, '//a[@class="Header_LogoScooter__3lsAR"]'          #Кнопка "Самокат"
+    COOKIE_BUTTON = By.XPATH, '//button[@id="rcc-confirm-button"]'                          #Кнопка принятия куки
+    FIRST_QUESTION = By.XPATH, '//div[@class="accordion"]/div[1]'                           #Первый вопрос на главной странице
+    SECOND_QUESTION = By.XPATH, '//div[@class="accordion"]/div[2]'                          #Второй вопрос на главной странице
+    THIRD_QUESTION = By.XPATH, '//div[@class="accordion"]/div[3]'                           #Третий вопрос на главной странице
+    FOURTH_QUESTION = By.XPATH, '//div[@id="accordion__heading-3"]'                         #Четвертый вопрос на главной странице
+    FIVETH_QUESTION = By.XPATH, '//div[@id="accordion__heading-4"]'                         #Пятый вопрос на главной странице
+    SIXTH_QUESTION = By.XPATH, '//div[@id="accordion__heading-5"]'                          #Шестой вопрос на главной странице
+    SEVENTH_QUESTION = By.XPATH, '//div[@id="accordion__heading-6"]'                        #Седьмой вопрос на главной странице
+    EIGHTH_QUESTION = By.XPATH, '//div[@id="accordion__heading-7"]'                         #Восьмой вопрос на главной странице
+    FIRST_ANSWER = By.XPATH, '//div[@class="accordion"]/div[1]//p'                          #Ответ на первый вопрос
+    SECOND_ANSWER = By.XPATH, '//div[@class="accordion"]/div[2]//p'                         #Ответ на второй вопрос
+    THIRD_ANSWER = By.XPATH, '//div[@class="accordion"]/div[3]//p'                          #Ответ на третий вопрос
+    FOURTH_ANSWER = By.XPATH, '//div[@class="accordion"]/div[4]//p'                         #Ответ на четвертый вопрос
+    FIVETH_ANSWER = By.XPATH, '//div[@class="accordion"]/div[5]//p'                         #Ответ на пятый вопрос
+    SIXTH_ANSWER = By.XPATH, '//div[@class="accordion"]/div[6]//p'                          #Ответ на шестой вопрос
+    SEVENTH_ANSWER = By.XPATH, '//div[@class="accordion"]/div[7]//p'                        #Ответ на седьмой вопрос
+    EIGHTH_ANSWER = By.XPATH, '//div[@class="accordion"]/div[8]//p'                         #Ответ на восьмой вопрос
+    TOP_ORDER_BUTTON = By.XPATH, '//button[@class="Button_Button__ra12g"]'                  #Кнопка "Заказать" сверху
+    BOTTOM_ORDER_BUTTON = By.XPATH, '//div[@class="Home_FinishButton__1_cWm"]/button'       #Кнопка "Заказать" снизу
+    DZEN_BUTTON = By.XPATH, '//a[@target="_blank"]'                                         #Кнопка "Дзен"
+    SCOOTER_MAIN_PAGE_BUTTON = By.XPATH, '//a[@class="Header_LogoScooter__3lsAR"]'          #Кнопка "Самокат"
 
 
 
--- src/locators/order_page_locators.py
+++ src/locators/order_page_locators.py
@@ -1,20 +1,20 @@
 from selenium.webdriver.common.by import By
 
 class OrderPageLocators:
-    first_name = By.XPATH, '//div[@class="Order_Form__17u6u"]/div[1]/input'         #Поле "Имя"
-    second_name = By.XPATH, '//div[@class="Order_Form__17u6u"]/div[2]/input'        #Поле "Фамилия"
-    address_locator = By.XPATH, '//div[@class="Order_Form__17u6u"]/div[3]/input'    #Поле "Адрес"
-    metro_station_field = By.XPATH, '//div[@class="select-search__value"]/input'    #Выпадающий список "Станция метро"
-    metro_station_variant = By.XPATH, '//li[1]'                                     #Один из вариантов в выпадающем списке "станция метро"
-    phone_number = By.XPATH, '//div[@class="Order_Form__17u6u"]/div[5]/input'       #Поле "Телефон"
-    next_button = By.XPATH, '//div[@class="Order_NextButton__1_rCA"]/button'        #Кнопка "Далее"
-    date = By.XPATH, '//div[@class="react-datepicker-wrapper"]//input'              #Поле "Когда привезти"
-    rental_period = By.XPATH, '//div[@class="Dropdown-control"]'                    #Поле "Срок аренды"
-    rent_variant1 = By.XPATH, '//div[@class="Dropdown-option"][1]'                  #"Срок аренды" : сутки
-    rent_variant2 = By.XPATH, '//div[@class="Dropdown-option"][2]'                  #"Срок аренды" : двое суток
-    black_color = By.XPATH, '//label[@for="black"]'                                 #"Цвет самоката" : черный жемчуг
-    grey_color = By.XPATH, '//label[@for="grey"]'                                   #"Цвет самоката" : серая безысходность
-    comment = By.XPATH, '//div[@class="Order_Form__17u6u"]/div/input'               #Поле "Комментарий"
-    order_button = By.XPATH, '//div[@class="Order_Buttons__1xGrp"]/button[2]'       #Кнопка "Заказать"
-    yes_button = By.XPATH, '//div[@class="Order_Modal__YZ-d3"]/div/button[2]'       #Кнопка "Да" на этапе согласия с заказом
-    order_placed_window = By.XPATH, '//div[@class="Order_Modal__YZ-d3"]'            #Окно с информацией об оформленном заказе
+    FIRST_NAME = By.XPATH, '//div[@class="Order_Form__17u6u"]/div[1]/input'         #Поле "Имя"
+    SECOND_NAME = By.XPATH, '//div[@class="Order_Form__17u6u"]/div[2]/input'        #Поле "Фамилия"
+    ADDRESS_LOCATOR = By.XPATH, '//div[@class="Order_Form__17u6u"]/div[3]/input'    #Поле "Адрес"
+    METRO_STATION_FIELD = By.XPATH, '//div[@class="select-search__value"]/input'    #Выпадающий список "Станция метро"
+    METRO_STATION_VARIANT = By.XPATH, '//li[1]'                                     #Один из вариантов в выпадающем списке "станция метро"
+    PHONE_NUMBER = By.XPATH, '//div[@class="Order_Form__17u6u"]/div[5]/input'       #Поле "Телефон"
+    NEXT_BUTTON = By.XPATH, '//div[@class="Order_NextButton__1_rCA"]/button'        #Кнопка "Далее"
+    DATE = By.XPATH, '//div[@class="react-datepicker-wrapper"]//input'              #Поле "Когда привезти"
+    RENTAL_PERIOD = By.XPATH, '//div[@class="Dropdown-control"]'                    #Поле "Срок аренды"
+    RENT_VARIANT1 = By.XPATH, '//div[@class="Dropdown-option"][1]'                  #"Срок аренды" : сутки
+    RENT_VARIANT2 = By.XPATH, '//div[@class="Dropdown-option"][2]'                  #"Срок аренды" : двое суток
+    BLACK_COLOR = By.XPATH, '//label[@for="black"]'                                 #"Цвет самоката" : черный жемчуг
+    GREY_COLOR = By.XPATH, '//label[@for="grey"]'                                   #"Цвет самоката" : серая безысходность
+    COMMENT = By.XPATH, '//div[@class="Order_Form__17u6u"]/div/input'               #Поле "Комментарий"
+    ORDER_BUTTON = By.XPATH, '//div[@class="Order_Buttons__1xGrp"]/button[2]'       #Кнопка "Заказать"
+    YES_BUTTON = By.XPATH, '//div[@class="Order_Modal__YZ-d3"]/div/button[2]'       #Кнопка "Да" на этапе согласия с заказом
+    ORDER_PLACED_WINDOW = By.XPATH, '//div[@class="Order_Modal__YZ-d3"]'            #Окно с информацией об оформленном заказе
